b7aefaeba2f1d8976d36e23ff883c3fb6da88914
samquindo <Quindolife66>
2019-08-11 17:43:40 +1200

    First commit

--- app/src/main/java/com/example/potential/MainActivity.java
+++ app/src/main/java/com/example/potential/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("First commit");
-        
+
         System.out.println("Second commit form github.");
+        System.out.println("hi");
     }
 }
