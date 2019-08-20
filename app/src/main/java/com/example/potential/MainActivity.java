64a6d82c394c20d19951151b6deaad188c08edcb
samquindo <Quindolife66>
2019-08-21 11:50:15 +1200

    master

--- app/src/main/java/com/example/potential/MainActivity.java
+++ app/src/main/java/com/example/potential/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Second commit fasdasdasdasdasdorm github");
         System.out.println("hisadasdasdasdasdasdasdasdasdasd");
 
-        System.out.println("maser");
+        System.out.println("masasdasdaser");
     }
 }
