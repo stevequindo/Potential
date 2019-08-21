d3d24a393f99b74ec46e36233e0a44f7be3a63e2
samquindo <Quindolife66>
2019-08-21 17:38:47 +1200

    Deleted everything to do with recycler view

--- app/src/main/java/com/example/potential/MainActivity.java
+++ app/src/main/java/com/example/potential/MainActivity.java
@@ -10,11 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("First commit");
 
-        System.out.println("Second commit fasdasdasdasdasdorm github");
-        System.out.println("hisadasdasdasdasdasdasdasdasdasd");
 
-        System.out.println("masasdasdaser");
+
     }
 }
